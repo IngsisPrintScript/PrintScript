b7937889c2460b35d475dc28049b688a564055d5
Santino2005 <santinocolombo13@gmail.com>
2025-08-07 15:10:45 -0300

    Added test to the MockParsers

--- src/test/java/LexicTest.java
+++ src/test/java/LexicTest.java
@@ -1,11 +1,31 @@
 import analyzers.Analyzer;
+import analyzers.lexic.LexicalAnalyzer;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import parsers.MockParser;
+import repositories.MockCodeRepository;
+import responses.CorrectResponse;
+import token.Token;
+
+import java.util.List;
+import java.util.Objects;
 
 public class LexicTest{
 
     @Test
     public void testSeparateWords(){
-       // Analyzer tokenizer = new Lexical(new TokenizerFactory());
+        MockCodeRepository mockCodeRepository = new MockCodeRepository("let number: String = \"Pepe\"; ");
+        MockParser mockParser = new MockParser(mockCodeRepository);
+        Assertions.assertTrue(mockParser.parse().isSuccessful());
+        Assertions.assertInstanceOf(CorrectResponse.class, mockParser.parse());
+        Assertions.assertEquals(mockParser.parse(), new CorrectResponse<>(List.of("let","number:","String","=","\"Pepe\";")));
+    }
+
+    @Test
+    public void testLexicalAnalyzer(){
+        MockCodeRepository mockCodeRepository = new MockCodeRepository(null);
+        MockParser mockParser = new MockParser(mockCodeRepository);
+        Assertions.assertFalse(mockParser.parse().isSuccessful());
     }
 
 }
