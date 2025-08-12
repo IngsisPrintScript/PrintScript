feea9cc2e282b909a9a98f08bf2f2694d1903969
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-12 20:57:26 -0300

    Implemnted LetNode

--- src/main/java/common/nodes/CompositeNode.java
+++ src/main/java/common/nodes/CompositeNode.java
@@ -2,13 +2,12 @@ package common.nodes;
 
 import common.responses.CorrectResult;
 import common.responses.Result;
-import common.visitor.VisitorInterface;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class CompositeNode implements Node {
-    private final List<Node> children;
+    protected final List<Node> children;
 
     public CompositeNode() {
         this.children = new ArrayList<>();
--- src/main/java/common/visitor/VisitorInterface.java
+++ src/main/java/common/visitor/VisitorInterface.java
@@ -1,4 +1,8 @@
 package common.visitor;
 
+import common.nodes.statements.LetStatementNode;
+import common.responses.Result;
+
 public interface VisitorInterface {
+    Result visit(LetStatementNode node);
 }
