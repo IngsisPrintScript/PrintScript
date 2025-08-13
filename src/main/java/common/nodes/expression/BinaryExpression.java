cdcf751176be4306b5d77fcb1053a2091b090309
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-12 21:04:24 -0300

    Implemented abstract class binary expression

---
+++ src/main/java/common/nodes/expression/BinaryExpression.java
@@ -0,0 +1,46 @@
+package common.nodes.expression;
+
+import common.nodes.CompositeNode;
+import common.nodes.Node;
+import common.responses.CorrectResult;
+import common.responses.IncorrectResult;
+import common.responses.Result;
+
+public abstract class BinaryExpression extends CompositeNode {
+    public Boolean hasLeftChild(){
+        try {
+            this.children.get(0);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public Boolean hasRightChild(){
+        try {
+            this.children.get(1);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    public Result leftChild(){
+        if (hasLeftChild()){
+            return new CorrectResult<>(this.children.get(0));
+        } else {
+            return new IncorrectResult("Binary expression has no left child.");
+        }
+    }
+    public Result rightChild(){
+        if (hasLeftChild()){
+            return new CorrectResult<>(this.children.get(1));
+        } else {
+            return new IncorrectResult("Binary expression has no right child.");
+        }
+    }
+    public Result addLeftChild(Node node){
+        return new CorrectResult<>(this.children.set(0, node));
+    }
+    public Result addRightChild(Node node){
+        return new CorrectResult<>(this.children.set(1, node));
+    }
+}
