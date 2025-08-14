b54fe1e48ea3a8e8bbe70d8d1520bd0cb32b5d3c
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-14 18:23:29 -0300

    Tested each individual tokenizer

---
+++ src/test/java/lexical/tokenizers/IdentifierTokenizerTest.java
@@ -0,0 +1,43 @@
+package lexical.tokenizers;
+
+import common.responses.CorrectResult;
+import common.responses.IncorrectResult;
+import common.responses.Result;
+import common.tokens.TokenInterface;
+import lexical.tokenizers.identifier.IdentifierTokenizer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class IdentifierTokenizerTest {
+    public static TokenizerInterface finalTokenizer;
+
+    @BeforeAll
+    public static void setUp(){
+        finalTokenizer = new FinalTokenizer();
+    }
+
+    @Test
+    public void creationIdentifierTokenizerTest(){
+        TokenizerInterface eolTokenizer = new IdentifierTokenizer(finalTokenizer);
+        Assertions.assertNotNull(eolTokenizer);
+    }
+
+    @Test
+    public void tokenizeIdentifierTokenizerTest(){
+        TokenizerInterface eolTokenizer = new IdentifierTokenizer(finalTokenizer);
+        // Asserting if the methods of the tokenizer work as expected
+        Assertions.assertFalse(eolTokenizer.canTokenize("%1"));
+        Assertions.assertTrue(eolTokenizer.canTokenize("validIdentifier"));
+        Assertions.assertInstanceOf(IncorrectResult.class, eolTokenizer.tokenize("%1"));
+        Assertions.assertInstanceOf(CorrectResult.class, eolTokenizer.tokenize("validIdentifier"));
+        Result tokenizeResult = eolTokenizer.tokenize("validIdentifier");
+        // Asserting the result is what is expected
+        Object object = ( (CorrectResult<?>)tokenizeResult).newObject();
+        Assertions.assertInstanceOf(TokenInterface.class, object);
+        TokenInterface token = (TokenInterface) object;
+        Assertions.assertNotNull(token);
+        Assertions.assertEquals("IDENTIFIER_TOKEN", token.name());
+        Assertions.assertEquals("validIdentifier", token.value());
+    }
+}
