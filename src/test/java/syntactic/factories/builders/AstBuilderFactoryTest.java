054d56af57c5a4ee44e441cda6fb4783a0af4c06
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-17 18:51:03 -0300

    Implemented tests for the builders factory

--- src/test/java/syntactic/factories/builders/AstBuilderFactoryTest.java
+++ src/test/java/syntactic/factories/builders/AstBuilderFactoryTest.java
@@ -3,10 +3,20 @@ package syntactic.factories.builders;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Method;
+
 public class AstBuilderFactoryTest {
     @Test
     public void createAstBuilderFactoryTest() {
         AstBuilderFactoryInterface astBuilderFactory = new AstBuilderFactory();
-        Assertions.assertNotNull(astBuilderFactory);
+        for (Method method: AstBuilderFactory.class.getDeclaredMethods()) {
+            if (!method.getName().startsWith("create")) continue;
+            try {
+                Object result = method.invoke(astBuilderFactory);
+                Assertions.assertNotNull(result);
+            } catch (Exception e){
+                Assertions.fail(e.getMessage());
+            }
+        }
     }
 }
