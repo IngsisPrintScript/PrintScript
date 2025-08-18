140750dca3d86dc1d07d0db9f2b61ad78b51e981
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-08-18 17:22:52 -0300

    Fixed the default java writer

--- src/main/java/interpreter/writer/DefaultJavaCodeWriter.java
+++ src/main/java/interpreter/writer/DefaultJavaCodeWriter.java
@@ -7,12 +7,20 @@ import common.responses.Result;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public record DefaultCodeWriter(Path path) implements CodeWriterInterface {
+public record DefaultJavaCodeWriter(Path path) implements CodeWriterInterface {
 
     @Override
     public Result writeCode(String code) {
         try {
-            Files.writeString(path, code);
+            String wrappedCode =
+                    """
+                    public class TranspiledCode {
+                        public static void main(String[] args) {
+                            %s
+                        }
+                    }
+                    """.formatted(code);
+            Files.writeString(path, wrappedCode);
             return new CorrectResult<>(path.toAbsolutePath().toString());
         } catch (Exception e) {
             return new IncorrectResult(e.getMessage());
