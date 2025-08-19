f35754242d37c77bbd73e85fd47dec80c9696f90
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-08-19 16:02:21 -0300

    Common module

--- common/src/main/java/common/nodes/Node.java
+++ common/src/main/java/common/nodes/Node.java
@@ -3,8 +3,6 @@ package common.nodes;
 import common.responses.Result;
 import common.visitor.VisitorInterface;
 
-import java.util.List;
-
 public interface Node {
     Result children();
     Result accept(VisitorInterface visitor);
