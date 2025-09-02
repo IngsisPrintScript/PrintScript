71afeb93c70db39638a04eaa2a4406f5c842c4ce
Santino2005 <santinocolombo13@gmail.com>
2025-09-01 23:11:03 -0300

    Finished the Formatter

--- formatter/src/main/java/formatter/FormatterVisitor.java
+++ formatter/src/main/java/formatter/FormatterVisitor.java
@@ -4,11 +4,13 @@ import common.NilNode;
 import declaration.AscriptionNode;
 import declaration.TypeNode;
 import expression.binary.AdditionNode;
+import expression.binary.AssignationNode;
 import expression.identifier.IdentifierNode;
 import expression.literal.LiteralNode;
 import formatter.FormatterRules.FactoryFormatterRules;
-import responses.CorrectResult;
-import responses.Result;
+import results.CorrectResult;
+import results.IncorrectResult;
+import results.Result;
 import statements.LetStatementNode;
 import statements.PrintStatementNode;
 import visitor.VisitorInterface;
@@ -26,13 +28,20 @@ public class FormatterVisitor implements VisitorInterface {
         StringBuilder sentence = new StringBuilder("let ");
         if(!node.hasExpression()){
             Result<String> declaration = this.visit(node.ascription().result());
-            return new CorrectResult<String>((sentence.append((String) declaration.result())).toString());
+            return new CorrectResult<String>((sentence
+                    .append(declaration.result())
+                    .append(";")
+                    .append("\n"))
+                    .toString());
 
         }
         Result<String> declaration = this.visit(node.ascription().result());
         Result<String> expression = node.expression().result().accept(this);
         return new CorrectResult<>((sentence
                 .append(declaration.result())
+                .append(formatterRules.SpaceBeforeAssignation())
+                .append("=")
+                .append(formatterRules.SpaceAfterAssignation())
                 .append(expression.result())
                 .append(";")
                 .append("\n"))
@@ -45,11 +54,12 @@ public class FormatterVisitor implements VisitorInterface {
         sentence.append(formatterRules.JumpsBeforePrint());
         Result<String> expression = node.expression().result().accept(this);
         return new CorrectResult<String>((sentence
-                .append("println(")
+                .append("println( ")
                 .append(expression.result())
-                .append(")")
+                .append(" )")
                 .append(";")
-                .append("\n")).toString());
+                .append("\n"))
+                .toString());
     }
 
     @Override
@@ -60,33 +70,43 @@ public class FormatterVisitor implements VisitorInterface {
         return new CorrectResult<>(sentence
                 .append(identifier.result())
                 .append(formatterRules.SpaceBeforeColon())
-                .append(type.result())
+                .append(":")
                 .append(formatterRules.SpaceAfterColon())
+                .append(type.result())
                 .toString());
     }
 
     @Override
     public Result<String> visit(AdditionNode node) {
-        return null;
+        Result<String> left = node.getLeftChild().result().accept(this);
+        Result<String> right = node.getRightChild().result().accept(this);
+        return new CorrectResult<>(left.result() + " + " + right.result());
+    }
+
+    @Override
+    public Result<String> visit(AssignationNode node) {
+        Result<String> right = node.getRightChild().result().accept(this);
+        Result<String> left = node.getLeftChild().result().accept(this);
+        return new CorrectResult<>(left.result() + formatterRules.SpaceBeforeAssignation() + "=" + formatterRules.SpaceAfterAssignation() + right.result() + ";" + "\n");
     }
 
     @Override
     public Result<String> visit(LiteralNode node) {
-        return null;
+        return new CorrectResult<>(node.value());
     }
 
     @Override
     public Result<String> visit(IdentifierNode node) {
-        return null;
+        return new CorrectResult<>(node.name());
     }
 
     @Override
     public Result<String> visit(TypeNode node) {
-        return null;
+        return new CorrectResult<>(node.type());
     }
 
     @Override
     public Result<String> visit(NilNode node) {
-        return null;
+        return new IncorrectResult<>("Cuack");
     }
 }
