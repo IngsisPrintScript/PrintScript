9ff686fbeccfa2693b944d6ae0d1111c86c8fa22
Santino2005 <santinocolombo13@gmail.com>
2025-09-02 01:34:41 -0300

    Finished Formatter

--- formatter/src/main/java/formatter/Formatter.java
+++ formatter/src/main/java/formatter/Formatter.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 
 public class Formatter {
     public String format(Node root) {
-        HashMap<FormatterRules, Boolean> rulesToFormat;
+        HashMap<FormatterRules, Boolean> rulesToFormat = new ReadRules().readRules();
         return root.accept(new FormatterVisitor(new FactoryFormatterRules(rulesToFormat))).result();
     }
 }
--- parser/src/main/java/parser/semantic/SemanticAnalyzer.java
+++ parser/src/main/java/parser/semantic/SemanticAnalyzer.java
@@ -4,7 +4,7 @@ import visitor.RuleVisitor;
 import visitor.SemanticallyCheckable;
 
 
-public record SemanticAnalyzer(RuleVisitor rulesEnforcer) implements SemanticInterface {
+public record SemanticAnalyzer(RuleVisitor rulesEnforcer) implements parser.semantic.SemanticInterface {
     @Override
     public Boolean isSemanticallyValid(SemanticallyCheckable tree) {
         return tree.acceptCheck(rulesEnforcer()).isSuccessful();
