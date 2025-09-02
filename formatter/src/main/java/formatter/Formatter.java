3e158483ad7e838a846de46516c86667d9ef6870
Santino2005 <santinocolombo13@gmail.com>
2025-09-01 23:25:02 -0300

    Finished the Formatter

--- formatter/src/main/java/formatter/Formatter.java
+++ formatter/src/main/java/formatter/Formatter.java
@@ -7,7 +7,8 @@ import formatter.FormatterRules.FormatterRules;
 import java.util.HashMap;
 
 public class Formatter {
-    public String format(Node root, HashMap<FormatterRules, Boolean> rulesToFormat) {
+    public String format(Node root) {
+        HashMap<FormatterRules, Boolean> rulesToFormat;
         return root.accept(new FormatterVisitor(new FactoryFormatterRules(rulesToFormat))).result();
     }
 }
