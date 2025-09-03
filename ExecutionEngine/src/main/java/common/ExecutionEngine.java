d42fbcdd5558668240cbbfc42f8e2a7492d626f2
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-09-03 11:51:54 -0300

    Made the interpreter depend on an Iterator<InterpretableNode> interface so that i revert the info flow

--- ExecutionEngine/src/main/java/common/ExecutionEngine.java
+++ ExecutionEngine/src/main/java/common/ExecutionEngine.java
@@ -26,30 +26,6 @@ public record ExecutionEngine(
 
     @Override
     public Result<String> execute() {
-        while (repository().hasMoreCode()) {
-            String code = repository.nextChunkOfCode();
-            Result<List<String>> parseResult = codeParser().parse(code);
-            if (!parseResult.isSuccessful()) {
-                return new IncorrectResult<>(parseResult.errorMessage());
-            }
-            List<String> tokens = parseResult.result();
-            Result<TokenStreamInterface> tokenizeResult = lexical().analyze(tokens);
-            if (!tokenizeResult.isSuccessful()) {
-                return new IncorrectResult<>(tokenizeResult.errorMessage());
-            }
-            TokenStreamInterface tokenStream = tokenizeResult.result();
-            Result<SemanticallyCheckable> buildAstResult = syntactic().buildAbstractSyntaxTree(tokenStream);
-            if (!buildAstResult.isSuccessful()) {
-                return new IncorrectResult<>(buildAstResult.errorMessage());
-            }
-            if (!semantic().isSemanticallyValid(buildAstResult.result())){
-                return new IncorrectResult<>("Semantic validation failed.");
-            }
-            Result<String> interpretResult = interpreter().interpreter((Node) buildAstResult.result());
-            if (!interpretResult.isSuccessful()) {
-                return new IncorrectResult<>(interpretResult.errorMessage());
-            }
-        }
-        return new CorrectResult<>("Execution of printscript program was successful.");
+        return new IncorrectResult<>("Not implemented yet.");
     }
 }
