0f82b2867f8a7d4dda71c43f9d28ce9c117126de
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-09-09 19:35:09 -0300

    Deleted tests

--- ExecutionEngine/src/main/java/factories/EngineFactoryInterface.java
+++
@@ -1,8 +0,0 @@
-package factories;
-
-import common.ExecutionEngineInterface;
-import repositories.CodeRepositoryInterface;
-
-public interface EngineFactoryInterface {
-    ExecutionEngineInterface getInterpreterEngine(CodeRepositoryInterface repository);
-}
