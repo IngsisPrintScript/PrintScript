02808795b308dae2be3fcd8fc67f6c5167af7409
Santino2005 <santinocolombo13@gmail.com>
2025-09-10 15:50:10 -0300

    testing

--- formatter/src/test/java/formatter/FormatterRulesTest.java
+++ formatter/src/test/java/formatter/FormatterRulesTest.java
@@ -1,31 +1,31 @@
-//package formatter;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-//import results.Result;
-//
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//
-//public class FormatterRulesTest {
-//
-//    @Test
-//    public void testFormatterCall() throws Exception {
-//        //Path inputPath = Path.of("/home/santiagogarrote/Faculty/Ingsis/PrintScript/formatter/src/main/resources/toFormat.txt");
-//        Path inputPath = Path.of("C:\\Users\\santi\\Faculty\\Ingsis\\PrintScript\\formatter\\src\\main\\resources\\toFormat.txt");
-//
-//        String originalContent = Files.readString(inputPath);
-//
-//        Formatter formatter = new Formatter();
-//        java.lang.reflect.Field field = Formatter.class.getDeclaredField("inputFile");
-//        field.setAccessible(true);
-//        field.set(formatter, inputPath);
-//
-//        Result<String> result = formatter.call();
-//
-//        String newContent = Files.readString(inputPath);
-//        Assertions.assertEquals(originalContent, newContent);
-//        System.out.println(newContent);
-//    }
-//
-//}
+package formatter;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import results.Result;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FormatterRulesTest {
+
+    @Test
+    public void testFormatterCall() throws Exception {
+        //Path inputPath = Path.of("/home/santiagogarrote/Faculty/Ingsis/PrintScript/formatter/src/main/resources/toFormat.txt");
+        Path inputPath = Path.of("C:\\Users\\santi\\Faculty\\Ingsis\\PrintScript\\formatter\\src\\main\\resources\\toFormat.txt");
+
+        String originalContent = Files.readString(inputPath);
+
+        Formatter formatter = new Formatter();
+        java.lang.reflect.Field field = Formatter.class.getDeclaredField("inputFile");
+        field.setAccessible(true);
+        field.set(formatter, inputPath);
+
+        Result<String> result = formatter.call();
+
+        String newContent = Files.readString(inputPath);
+        Assertions.assertEquals(originalContent, newContent);
+        System.out.println(newContent);
+    }
+
+}
