7f1dc4f718a3d1bdfc304d16f04d86da7993433d
Santino2005 <santinocolombo13@gmail.com>
2025-09-13 12:26:13 -0300

    Implemented the tokenizers

--- lexer/src/main/java/com/ingsis/printscript/tokenizers/keyword/v1/IfKeywordTokenizer.java
+++ lexer/src/main/java/com/ingsis/printscript/tokenizers/keyword/v1/IfKeywordTokenizer.java
@@ -1,30 +1,28 @@
-package com.ingsis.printscript.tokenizers.keyword;
+package com.ingsis.printscript.tokenizers.keyword.v1;
 
 import com.ingsis.printscript.results.CorrectResult;
 import com.ingsis.printscript.results.Result;
 import com.ingsis.printscript.tokenizers.FinalTokenizer;
 import com.ingsis.printscript.tokenizers.TokenizerInterface;
+import com.ingsis.printscript.tokenizers.keyword.KeywordTokenizer;
 import com.ingsis.printscript.tokens.TokenInterface;
 import com.ingsis.printscript.tokens.factories.TokenFactory;
 
-public class ConstKeywordTokenizer extends  KeywordTokenizer {
-
-    public ConstKeywordTokenizer() {
+public class IfKeywordTokenizer extends KeywordTokenizer {
+    public IfKeywordTokenizer() {
         super(new FinalTokenizer());
     }
-
-    public ConstKeywordTokenizer(TokenizerInterface nextTokenizer) {
+    public IfKeywordTokenizer(TokenizerInterface nextTokenizer) {
         super(nextTokenizer);
     }
 
     @Override
     public Boolean canTokenize(String input) {
-        return input.equals("const");
+        return input.equals("if");
     }
 
     @Override
     public Result<TokenInterface> tokenize(String input) {
         return new CorrectResult<>(new TokenFactory().createLetKeywordToken());
     }
-
 }
---
+++ lexer/src/main/java/com/ingsis/printscript/tokenizers/punctuation/v1brackets/RightBracketTokenizer.java
@@ -0,0 +1,23 @@
+package com.ingsis.printscript.tokenizers.punctuation.v1brackets;
+
+import com.ingsis.printscript.results.CorrectResult;
+import com.ingsis.printscript.results.IncorrectResult;
+import com.ingsis.printscript.results.Result;
+import com.ingsis.printscript.tokenizers.punctuation.PunctuationTokenizer;
+import com.ingsis.printscript.tokens.TokenInterface;
+import com.ingsis.printscript.tokens.factories.TokenFactory;;
+
+public class RightBracketTokenizer extends PunctuationTokenizer {
+
+    @Override
+    public Boolean canTokenize(String input) {
+        return input.equals("}");
+    }
+
+    @Override
+    public Result<TokenInterface> tokenize(String input) {
+        if (!canTokenize(input)) {return new IncorrectResult<>("Cannot tokenize provided input");}
+        return new CorrectResult<>(new TokenFactory().createRightParenthesisToken());
+    }
+
+}
