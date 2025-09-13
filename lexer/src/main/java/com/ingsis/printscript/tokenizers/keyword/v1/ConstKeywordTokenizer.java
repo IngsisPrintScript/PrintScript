d37760997b7e7da5c76d6242810231c4f642b702
Santino2005 <santinocolombo13@gmail.com>
2025-09-13 12:26:13 -0300

    Implemented the tokenizers

---
+++ lexer/src/main/java/com/ingsis/printscript/tokenizers/keyword/v1/ConstKeywordTokenizer.java
@@ -0,0 +1,31 @@
+package com.ingsis.printscript.tokenizers.keyword.v1;
+
+import com.ingsis.printscript.results.CorrectResult;
+import com.ingsis.printscript.results.Result;
+import com.ingsis.printscript.tokenizers.FinalTokenizer;
+import com.ingsis.printscript.tokenizers.TokenizerInterface;
+import com.ingsis.printscript.tokenizers.keyword.KeywordTokenizer;
+import com.ingsis.printscript.tokens.TokenInterface;
+import com.ingsis.printscript.tokens.factories.TokenFactory;
+
+public final class ConstKeywordTokenizer extends KeywordTokenizer {
+
+    public ConstKeywordTokenizer() {
+        super(new FinalTokenizer());
+    }
+
+    public ConstKeywordTokenizer(TokenizerInterface nextTokenizer) {
+        super(nextTokenizer);
+    }
+
+    @Override
+    public Boolean canTokenize(String input) {
+        return input.equals("const");
+    }
+
+    @Override
+    public Result<TokenInterface> tokenize(String input) {
+        return new CorrectResult<>(new TokenFactory().createLetKeywordToken());
+    }
+
+}
