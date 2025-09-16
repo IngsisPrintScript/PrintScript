e543fc22fb7a618c2370b01aebda3325b1690afa
GarroteSantiago <sgarrote@mail.austral.edu.ar>
2025-09-16 17:26:55 -0300

    Removed from cd the env

--- cli-app/.testFiles/interpret/ClassTest.java
+++
@@ -1,7 +0,0 @@
-public class ClassTest {
-    public static void main(String[] args) {
-        String varOne = "varA";
-        String varTwo = "varB";
-    System.out.println(varOne + varTwo);
-}
-}
