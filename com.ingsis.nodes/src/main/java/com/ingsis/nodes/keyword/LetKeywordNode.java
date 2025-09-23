93d0f31f7dc30f075014275a050b38ef648a08a8
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-09-23 18:44:55 -0300

    Removed the flow logic from node

--- com.ingsis.nodes/src/main/java/com/ingsis/nodes/keyword/LetKeywordNode.java
+++ com.ingsis.nodes/src/main/java/com/ingsis/nodes/keyword/LetKeywordNode.java
@@ -7,8 +7,6 @@ package com.ingsis.nodes.keyword;
 import com.ingsis.nodes.Node;
 import com.ingsis.nodes.operator.TypeAssignationNode;
 import com.ingsis.nodes.operator.ValueAssignationNode;
-import com.ingsis.result.CorrectResult;
-import com.ingsis.result.IncorrectResult;
 import com.ingsis.result.Result;
 import com.ingsis.visitors.Checker;
 import com.ingsis.visitors.Interpreter;
@@ -20,37 +18,12 @@ public record LetKeywordNode(
 
     @Override
     public Result<String> acceptChecker(Checker checker) {
-        Result<String> interpretThis = checker.check(this);
-        if (!interpretThis.isCorrect()) {
-            return new IncorrectResult<>(interpretThis);
-        }
-        Result<String> interpretTypeAssignation = checker.check(this.typeAssignationNode());
-        if (!interpretTypeAssignation.isCorrect()) {
-            return new IncorrectResult<>(interpretTypeAssignation);
-        }
-        Result<String> interpretValueAssignation = checker.check(this.valueAssignationNode());
-        if (!interpretValueAssignation.isCorrect()) {
-            return new IncorrectResult<>(interpretValueAssignation);
-        }
-        return new CorrectResult<>("Successfully interpreted declaration");
+        return checker.check(this);
     }
 
     @Override
     public Result<String> acceptInterpreter(Interpreter interpreter) {
-        Result<String> interpretThis = interpreter.interpret(this);
-        if (!interpretThis.isCorrect()) {
-            return new IncorrectResult<>(interpretThis);
-        }
-        Result<String> interpretTypeAssignation = interpreter.interpret(this.typeAssignationNode());
-        if (!interpretTypeAssignation.isCorrect()) {
-            return new IncorrectResult<>(interpretTypeAssignation);
-        }
-        Result<String> interpretValueAssignation =
-                interpreter.interpret(this.valueAssignationNode());
-        if (!interpretValueAssignation.isCorrect()) {
-            return new IncorrectResult<>(interpretValueAssignation);
-        }
-        return new CorrectResult<>("Successfully interpreted declaration");
+        return interpreter.interpret(this);
     }
 
     @Override
