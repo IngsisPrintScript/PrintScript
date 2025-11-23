41c18845bdd4aeb61378db91088fc1782449729e
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 16:11:50 -0300

    implemented tets for engine/factories/tokenstream

---
+++ com.ingsis.engine/src/test/java/com/ingsis/engine/factories/tokenstream/TokenStreamFactoryTest.java
@@ -0,0 +1,12 @@
+package com.ingsis.engine.factories.tokenstream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class TokenStreamFactoryTest {
+    @Test
+    void isInterface() {
+        assertTrue(TokenStreamFactory.class.isInterface());
+    }
+}
