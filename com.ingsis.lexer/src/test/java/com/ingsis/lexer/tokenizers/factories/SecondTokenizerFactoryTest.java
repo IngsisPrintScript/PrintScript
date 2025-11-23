3f6c4eeb4d49acb9c10a149a7044c02e7f9272ef
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 20:38:16 -0300

    implemented tests for lexer/tokenizers/factories

---
+++ com.ingsis.lexer/src/test/java/com/ingsis/lexer/tokenizers/factories/SecondTokenizerFactoryTest.java
@@ -0,0 +1,33 @@
+package com.ingsis.lexer.tokenizers.factories;
+
+import com.ingsis.lexer.TestUtils;
+import com.ingsis.result.Result;
+import com.ingsis.result.factory.ResultFactory;
+import com.ingsis.tokens.Token;
+import com.ingsis.tokens.factories.TokenFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SecondTokenizerFactoryTest {
+    private TokenFactory tokenFactory;
+    private ResultFactory resultFactory;
+    private SecondTokenizerFactory factory;
+
+    @BeforeEach
+    void setUp() {
+        tokenFactory = TestUtils.tokenFactory();
+        resultFactory = TestUtils.resultFactory();
+        factory = new SecondTokenizerFactory(tokenFactory, resultFactory);
+    }
+
+    @Test
+    void createTokenizer_canTokenizeKeywordsAndBoolean() {
+        var tokenizer = factory.createTokenizer();
+        Result<Token> r1 = tokenizer.tokenize("if", 1, 1);
+        assertTrue(r1.isCorrect());
+        Result<Token> r2 = tokenizer.tokenize("false", 1, 1);
+        assertTrue(r2.isCorrect());
+    }
+}
