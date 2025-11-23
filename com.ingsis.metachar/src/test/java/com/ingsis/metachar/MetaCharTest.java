f556d1dfbb6b13cd84975a812b04ccd9cd0bb16c
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 20:47:54 -0300

    implemented tests for metachar module

---
+++ com.ingsis.metachar/src/test/java/com/ingsis/metachar/MetaCharTest.java
@@ -0,0 +1,43 @@
+package com.ingsis.metachar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class MetaCharTest {
+
+    private MetaChar a;
+    private MetaChar b;
+
+    @BeforeEach
+    public void setUp() {
+        a = new MetaChar('H', 1, 1);
+        b = new MetaChar('i', 1, 2);
+    }
+
+    @Test
+    public void accessorsReturnProvidedValues() {
+        assertEquals(Character.valueOf('H'), a.character());
+        assertEquals(Integer.valueOf(1), a.line());
+        assertEquals(Integer.valueOf(1), a.column());
+    }
+
+    @Test
+    public void equalsAndHashCodeRespectState() {
+        MetaChar copy = new MetaChar('H', 1, 1);
+        assertEquals(a, copy);
+        assertEquals(a.hashCode(), copy.hashCode());
+    }
+
+    @Test
+    public void supportsNullValues() {
+        MetaChar nullChar = new MetaChar(null, null, null);
+        assertNull(nullChar.character());
+        assertNull(nullChar.line());
+        assertNull(nullChar.column());
+        assertNotNull(nullChar.toString());
+    }
+}
---
+++ com.ingsis.metachar/src/test/java/com/ingsis/metachar/string/builder/MetaCharStringBuilderTest.java
@@ -0,0 +1,76 @@
+package com.ingsis.metachar.string.builder;
+
+import com.ingsis.metachar.MetaChar;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MetaCharStringBuilderTest {
+
+    private MetaChar m1;
+    private MetaChar m2;
+    private MetaChar m3;
+
+    @BeforeEach
+    public void setUp() {
+        m1 = new MetaChar('H', 1, 1);
+        m2 = new MetaChar('i', 1, 2);
+        m3 = new MetaChar('!', 2, 1);
+    }
+
+    @Test
+    public void emptyConstructorInitializesNullLineAndColumn() {
+        MetaCharStringBuilder builder = new MetaCharStringBuilder();
+        assertNull(builder.getLine());
+        assertNull(builder.getColumn());
+        assertEquals("", builder.getString());
+
+        builder.append(m1);
+        assertEquals(Integer.valueOf(1), builder.getLine());
+        assertEquals(Integer.valueOf(1), builder.getColumn());
+        assertEquals("H", builder.getString());
+    }
+
+    @Test
+    public void constructorWithInitialListSetsLineColumnAndString() {
+        List<MetaChar> initial = List.of(m1, m2);
+        MetaCharStringBuilder builder = new MetaCharStringBuilder(initial);
+        assertEquals(Integer.valueOf(1), builder.getLine());
+        assertEquals(Integer.valueOf(1), builder.getColumn());
+        assertEquals("Hi", builder.getString());
+
+        builder.append(m3);
+        assertEquals("Hi!", builder.getString());
+        assertEquals(Integer.valueOf(1), builder.getLine());
+        assertEquals(Integer.valueOf(1), builder.getColumn());
+    }
+
+    @Test
+    public void appendSetsLineAndColumnWhenPreviouslyNull() {
+        MetaCharStringBuilder builder = new MetaCharStringBuilder(new ArrayList<>());
+        assertNull(builder.getLine());
+        assertNull(builder.getColumn());
+
+        builder.append(m3);
+        assertNotNull(builder.getLine());
+        assertNotNull(builder.getColumn());
+        assertEquals(Integer.valueOf(2), builder.getLine());
+        assertEquals(Integer.valueOf(1), builder.getColumn());
+        assertEquals("!", builder.getString());
+    }
+
+    @Test
+    public void getStringConcatenatesAllCharacters() {
+        MetaCharStringBuilder builder = new MetaCharStringBuilder();
+        builder.append(m1);
+        builder.append(m2);
+        builder.append(m3);
+        assertEquals("Hi!", builder.getString());
+    }
+}
