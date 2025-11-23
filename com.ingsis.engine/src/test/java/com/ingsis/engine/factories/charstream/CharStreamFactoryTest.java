9cecf555af307ce3cac4579eefe295c7959bd2ad
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 16:10:05 -0300

    implemented tets for engine/factories/charstream

---
+++ com.ingsis.engine/src/test/java/com/ingsis/engine/factories/charstream/CharStreamFactoryTest.java
@@ -0,0 +1,12 @@
+package com.ingsis.engine.factories.charstream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class CharStreamFactoryTest {
+    @Test
+    void isInterface() {
+        assertTrue(CharStreamFactory.class.isInterface());
+    }
+}
---
+++ com.ingsis.engine/src/test/java/com/ingsis/engine/factories/charstream/DefaultCharStreamFactoryTest.java
@@ -0,0 +1,45 @@
+package com.ingsis.engine.factories.charstream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.ingsis.metachar.MetaChar;
+import com.ingsis.peekableiterator.PeekableIterator;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DefaultCharStreamFactoryTest {
+    private DefaultCharStreamFactory factory;
+
+    @BeforeEach
+    void setup() {
+        factory = new DefaultCharStreamFactory();
+    }
+
+    @Test
+    void inMemoryCharIteratorReturnsIterator() {
+        Queue<Character> buffer = new ArrayDeque<>();
+        buffer.add('a');
+        PeekableIterator<MetaChar> it = factory.inMemoryCharIterator(buffer);
+        assertNotNull(it);
+        assertTrue(it.hasNext());
+        MetaChar mc = it.next();
+        assertEquals(Character.valueOf('a'), mc.character());
+    }
+
+    @Test
+    void fromFileCharIteratorReadsFile() throws IOException {
+        Path tmp = Files.createTempFile("ps-test", ".txt");
+        Files.writeString(tmp, "z");
+        PeekableIterator<MetaChar> it = factory.fromFileCharIterator(tmp);
+        assertNotNull(it);
+        assertTrue(it.hasNext());
+        MetaChar mc = it.next();
+        assertEquals(Character.valueOf('z'), mc.character());
+        Files.deleteIfExists(tmp);
+    }
+}
