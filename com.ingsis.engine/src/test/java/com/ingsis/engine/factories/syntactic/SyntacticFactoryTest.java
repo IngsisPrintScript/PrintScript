5ae535ea05cd52ff41d44f20ff749831141a5576
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 16:11:37 -0300

    implemented tets for engine/factories/syntactic

---
+++ com.ingsis.engine/src/test/java/com/ingsis/engine/factories/syntactic/SyntacticFactoryTest.java
@@ -0,0 +1,12 @@
+package com.ingsis.engine.factories.syntactic;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SyntacticFactoryTest {
+    @Test
+    void isInterface() {
+        assertTrue(SyntacticFactory.class.isInterface());
+    }
+}
