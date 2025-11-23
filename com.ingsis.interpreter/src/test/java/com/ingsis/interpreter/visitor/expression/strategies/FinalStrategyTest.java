cbf8ace58b3b3bdf4fac49446d67368220b876da
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2025-11-23 20:03:18 -0300

    implemented tests for interpreter/visitor

---
+++ com.ingsis.interpreter/src/test/java/com/ingsis/interpreter/visitor/expression/strategies/FinalStrategyTest.java
@@ -0,0 +1,17 @@
+package com.ingsis.interpreter.visitor.expression.strategies;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import com.ingsis.nodes.expression.literal.LiteralNode;
+import com.ingsis.result.Result;
+import com.ingsis.visitors.Interpreter;
+import org.junit.jupiter.api.Test;
+
+class FinalStrategyTest {
+    @Test
+    void returnsIncorrectForAnyExpression() {
+        FinalStrategy s = new FinalStrategy();
+        Result<Object> r = s.solve((Interpreter) null, new LiteralNode("x", 0, 0));
+        assertFalse(r.isCorrect());
+    }
+}
