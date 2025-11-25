de5622f115b56b1be459afb3762b9c5084036867
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-11-25 05:35:16 -0300

    Fixed types keywords

--- com.ingsis.types/src/main/java/com/ingsis/types/Types.java
+++ com.ingsis.types/src/main/java/com/ingsis/types/Types.java
@@ -8,52 +8,53 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Types {
-    NUMBER("Number", "\\d+(\\.\\d+)?"),
-    BOOLEAN("Boolean", "true|false"),
-    NIL("Nil", "nil"),
-    UNDEFINED("Undefined", "undefined"),
-    STRING("String", "([^\"\\\\]|\\\\.)*");
+  NUMBER("number", "\\d+(\\.\\d+)?"),
+  BOOLEAN("boolean", "true|false"),
+  NIL("nil", "nil"),
+  UNDEFINED("undefined", "undefined"),
+  STRING("string", "([^\"\\\\]|\\\\.)*");
 
-    private final String keyword;
-    private final String regEx;
+  private final String keyword;
+  private final String regEx;
 
-    Types(String keyword, String regex) {
-        this.keyword = keyword;
-        this.regEx = regex;
+  Types(String keyword, String regex) {
+    this.keyword = keyword;
+    this.regEx = regex;
+  }
+
+  public String keyword() {
+    return keyword;
+  }
+
+  public String regEx() {
+    return regEx;
+  }
+
+  public boolean isCompatibleWith(Object object) {
+    String string = object.toString();
+    return this.checkFormat(string);
+  }
+
+  public boolean isCompatibleWith(Types other) {
+    if (this == UNDEFINED || other == UNDEFINED)
+      return true;
+    return this.equals(other);
+  }
+
+  public boolean checkFormat(String input) {
+    return input.matches(this.regEx());
+  }
+
+  public static Types fromKeyword(String input) {
+    for (Types t : values()) {
+      if (t.keyword.equals(input)) {
+        return t;
+      }
     }
+    return UNDEFINED;
+  }
 
-    public String keyword() {
-        return keyword;
-    }
-
-    public String regEx() {
-        return regEx;
-    }
-
-    public boolean isCompatibleWith(Object object) {
-        String string = object.toString();
-        return this.checkFormat(string);
-    }
-
-    public boolean isCompatibleWith(Types other) {
-        if (this == UNDEFINED || other == UNDEFINED) return true;
-        return this.equals(other);
-    }
-
-    public boolean checkFormat(String input) {
-        return input.matches(this.regEx());
-    }
-
-    public static Types fromKeyword(String input) {
-        for (Types t : values()) {
-            if (t.keyword.equals(input)) {
-                return t;
-            }
-        }
-        return UNDEFINED;
-    }
-
-    public static List<Types> allTypes() {
-        return List.copyOf(Arrays.asList(values()));
-    }
+  public static List<Types> allTypes() {
+    return List.copyOf(Arrays.asList(values()));
+  }
 }
