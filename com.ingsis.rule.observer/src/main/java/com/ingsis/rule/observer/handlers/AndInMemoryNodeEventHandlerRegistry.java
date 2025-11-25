b389831ee4acdde027e9bcec605d13c4b31c9b4f
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-11-25 04:54:18 -0300

    Fixed inmmutability error

--- com.ingsis.rule.observer/src/main/java/com/ingsis/rule/observer/handlers/AndInMemoryNodeEventHandlerRegistry.java
+++ com.ingsis.rule.observer/src/main/java/com/ingsis/rule/observer/handlers/AndInMemoryNodeEventHandlerRegistry.java
@@ -11,33 +11,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AndInMemoryNodeEventHandlerRegistry<T extends Node>
-        implements NodeEventHandlerRegistry<T> {
-    private final List<NodeEventHandler<T>> handlers;
-    private final ResultFactory resultFactory;
+    implements NodeEventHandlerRegistry<T> {
+  private final List<NodeEventHandler<T>> handlers;
+  private final ResultFactory resultFactory;
 
-    public AndInMemoryNodeEventHandlerRegistry(
-            List<NodeEventHandler<T>> handlers, ResultFactory resultFactory) {
-        this.handlers = List.copyOf(handlers);
-        this.resultFactory = resultFactory;
-    }
+  public AndInMemoryNodeEventHandlerRegistry(
+      List<NodeEventHandler<T>> handlers, ResultFactory resultFactory) {
+    this.handlers = new ArrayList<>(handlers);
+    this.resultFactory = resultFactory;
+  }
 
-    public AndInMemoryNodeEventHandlerRegistry(ResultFactory resultFactory) {
-        this(new ArrayList<>(), resultFactory);
-    }
+  public AndInMemoryNodeEventHandlerRegistry(ResultFactory resultFactory) {
+    this(new ArrayList<>(), resultFactory);
+  }
 
-    @Override
-    public Result<String> handle(T node) {
-        for (NodeEventHandler<T> handler : handlers) {
-            Result<String> handleResult = handler.handle(node);
-            if (!handleResult.isCorrect()) {
-                return resultFactory.cloneIncorrectResult(handleResult);
-            }
-        }
-        return resultFactory.createCorrectResult("All checks passed.");
+  @Override
+  public Result<String> handle(T node) {
+    for (NodeEventHandler<T> handler : handlers) {
+      Result<String> handleResult = handler.handle(node);
+      if (!handleResult.isCorrect()) {
+        return resultFactory.cloneIncorrectResult(handleResult);
+      }
     }
+    return resultFactory.createCorrectResult("All checks passed.");
+  }
 
-    @Override
-    public void register(NodeEventHandler<T> newHandler) {
-        this.handlers.add(newHandler);
-    }
+  @Override
+  public void register(NodeEventHandler<T> newHandler) {
+    this.handlers.add(newHandler);
+  }
 }
