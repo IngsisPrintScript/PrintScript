8690f8263db3e03c490116ea990695d4bfee775f
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-12-08 22:46:01 -0300

    .

--- com.ingsis.parser/src/main/java/com/ingsis/parser/syntactic/factories/SyntacticFactory.java
+++ com.ingsis.parser/src/main/java/com/ingsis/parser/syntactic/factories/SyntacticFactory.java
@@ -12,35 +12,47 @@ import com.ingsis.utils.nodes.visitors.Checkable;
 import com.ingsis.utils.result.factory.ResultFactory;
 import com.ingsis.utils.token.Token;
 import com.ingsis.utils.token.factories.TokenFactory;
+import com.ingsis.utils.token.template.factories.DefaultTokenTemplateFactory;
+import com.ingsis.utils.token.template.factories.TokenTemplateFactory;
 import com.ingsis.utils.token.tokenstream.DefaultTokenStream;
+import com.ingsis.utils.token.type.TokenType;
+
 import java.io.InputStream;
+import java.util.List;
 
 public final class SyntacticFactory implements SafeIteratorFactory<Checkable> {
-    private final SafeIteratorFactory<Token> tokenIteratorFactory;
-    private final ParserChainFactory parserChainFactory;
-    private final IterationResultFactory iterationResultFactory;
-    private final TokenFactory tokenFactory;
-    private final ResultFactory resultFactory;
+  private final SafeIteratorFactory<Token> tokenIteratorFactory;
+  private final ParserChainFactory parserChainFactory;
+  private final IterationResultFactory iterationResultFactory;
+  private final TokenFactory tokenFactory;
+  private final ResultFactory resultFactory;
 
-    public SyntacticFactory(
-            SafeIteratorFactory<Token> tokenIteratorFactory,
-            ParserChainFactory parserFactory,
-            IterationResultFactory iterationResultFactory,
-            TokenFactory tokenFactory,
-            ResultFactory resultFactory) {
-        this.tokenIteratorFactory = tokenIteratorFactory;
-        this.parserChainFactory = parserFactory;
-        this.iterationResultFactory = iterationResultFactory;
-        this.tokenFactory = tokenFactory;
-        this.resultFactory = resultFactory;
-    }
+  public SyntacticFactory(
+      SafeIteratorFactory<Token> tokenIteratorFactory,
+      ParserChainFactory parserFactory,
+      IterationResultFactory iterationResultFactory,
+      TokenFactory tokenFactory,
+      ResultFactory resultFactory) {
+    this.tokenIteratorFactory = tokenIteratorFactory;
+    this.parserChainFactory = parserFactory;
+    this.iterationResultFactory = iterationResultFactory;
+    this.tokenFactory = tokenFactory;
+    this.resultFactory = resultFactory;
+  }
 
-    @Override
-    public SafeIterator<Checkable> fromInputStream(InputStream in) {
-        return new SyntacticParser(
-                tokenIteratorFactory.fromInputStream(in),
-                parserChainFactory.createDefaultChain(),
-                new DefaultTokenStream(tokenFactory, iterationResultFactory, resultFactory),
-                iterationResultFactory);
-    }
+  @Override
+  public SafeIterator<Checkable> fromInputStream(InputStream in) {
+    TokenTemplateFactory tokenTemplateFactory = new DefaultTokenTemplateFactory();
+    return new SyntacticParser(
+        tokenIteratorFactory.fromInputStream(in),
+        parserChainFactory.createDefaultChain(),
+        new DefaultTokenStream(
+            List.of(tokenTemplateFactory.separator(TokenType.SPACE.lexeme()).result(),
+                tokenTemplateFactory.separator(TokenType.NEWLINE.lexeme()).result(),
+                tokenTemplateFactory.separator(TokenType.TAB.lexeme()).result(),
+                tokenTemplateFactory.separator(TokenType.CRETURN.lexeme()).result()),
+            tokenFactory,
+            iterationResultFactory, resultFactory),
+        iterationResultFactory);
+  }
 }
--- com.ingsis.parser/src/main/java/com/ingsis/parser/syntactic/parsers/operator/algorithms/postfix/PostfixExpressionTreeBuilder.java
+++ com.ingsis.parser/src/main/java/com/ingsis/parser/syntactic/parsers/operator/algorithms/postfix/PostfixExpressionTreeBuilder.java
@@ -18,8 +18,12 @@ import com.ingsis.utils.result.Result;
 import com.ingsis.utils.result.factory.ResultFactory;
 import com.ingsis.utils.token.Token;
 import com.ingsis.utils.token.factories.TokenFactory;
+import com.ingsis.utils.token.template.factories.DefaultTokenTemplateFactory;
+import com.ingsis.utils.token.template.factories.TokenTemplateFactory;
 import com.ingsis.utils.token.tokenstream.DefaultTokenStream;
 import com.ingsis.utils.token.tokenstream.TokenStream;
+import com.ingsis.utils.token.type.TokenType;
+
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
@@ -27,83 +31,85 @@ import java.util.List;
 import java.util.Queue;
 
 public class PostfixExpressionTreeBuilder implements PostfixToAstBuilder {
-    private final NodeFactory nodeFactory;
-    private final TokenFactory tokenFactory;
-    private final IterationResultFactory iterationResultFactory;
-    private final ResultFactory resultFactory;
+  private final NodeFactory nodeFactory;
+  private final TokenFactory tokenFactory;
+  private final IterationResultFactory iterationResultFactory;
+  private final ResultFactory resultFactory;
 
-    public PostfixExpressionTreeBuilder(
-            NodeFactory nodeFactory,
-            TokenFactory tokenFactory,
-            IterationResultFactory iterationResultFactory,
-            ResultFactory resultFactory) {
-        this.nodeFactory = nodeFactory;
-        this.tokenFactory = tokenFactory;
-        this.iterationResultFactory = iterationResultFactory;
-        this.resultFactory = resultFactory;
-    }
+  public PostfixExpressionTreeBuilder(
+      NodeFactory nodeFactory,
+      TokenFactory tokenFactory,
+      IterationResultFactory iterationResultFactory,
+      ResultFactory resultFactory) {
+    this.nodeFactory = nodeFactory;
+    this.tokenFactory = tokenFactory;
+    this.iterationResultFactory = iterationResultFactory;
+    this.resultFactory = resultFactory;
+  }
 
-    @Override
-    public ProcessCheckpoint<Token, ExpressionNode> build(
-            Parser<ExpressionNode> leafExpressionNodesParser, Queue<Token> postfixTokens) {
-        return parsePostFix(leafExpressionNodesParser, postfixTokens);
-    }
+  @Override
+  public ProcessCheckpoint<Token, ExpressionNode> build(
+      Parser<ExpressionNode> leafExpressionNodesParser, Queue<Token> postfixTokens) {
+    return parsePostFix(leafExpressionNodesParser, postfixTokens);
+  }
 
-    private ProcessCheckpoint<Token, ExpressionNode> parsePostFix(
-            Parser<ExpressionNode> leafExpressionNodesParser, Queue<Token> postfixTokens) {
-        Deque<ExpressionNode> expressionStack = new ArrayDeque<>();
-        TokenStream tokenStream =
-                new DefaultTokenStream(
-                        List.copyOf(postfixTokens),
-                        tokenFactory,
-                        iterationResultFactory,
-                        resultFactory);
-        SafeIterationResult<Token> iterationResult = tokenStream.next();
-        while (iterationResult.isCorrect()) {
-            Token token = iterationResult.iterationResult();
-            Result<OperatorType> opResult = OperatorType.fromSymbol(token.value());
-            if (opResult.isCorrect()) {
-                Result<Deque<ExpressionNode>> newStackResult =
-                        generateOperatorNode(opResult.result(), expressionStack);
-                if (!newStackResult.isCorrect()) {
-                    return ProcessCheckpoint.UNINITIALIZED();
-                }
-                expressionStack = newStackResult.result();
-            } else {
-                ProcessCheckpoint<Token, ProcessResult<ExpressionNode>> leafResult =
-                        leafExpressionNodesParser.parse(tokenStream);
-                if (leafResult.isUninitialized()) {
-                    return ProcessCheckpoint.UNINITIALIZED();
-                }
-
-                if (leafResult.result().isComplete()) {
-                    expressionStack.push(leafResult.result().result());
-                    tokenStream = (TokenStream) leafResult.iterator();
-                } else {
-                    break;
-                }
-            }
-            iterationResult = tokenStream.next();
+  private ProcessCheckpoint<Token, ExpressionNode> parsePostFix(
+      Parser<ExpressionNode> leafExpressionNodesParser, Queue<Token> postfixTokens) {
+    Deque<ExpressionNode> expressionStack = new ArrayDeque<>();
+    TokenTemplateFactory tokenTemplateFactory = new DefaultTokenTemplateFactory();
+    TokenStream tokenStream = new DefaultTokenStream(
+        List.copyOf(postfixTokens),
+        List.of(tokenTemplateFactory.separator(TokenType.SPACE.lexeme()).result(),
+            tokenTemplateFactory.separator(TokenType.NEWLINE.lexeme()).result(),
+            tokenTemplateFactory.separator(TokenType.TAB.lexeme()).result(),
+            tokenTemplateFactory.separator(TokenType.CRETURN.lexeme()).result()),
+        tokenFactory,
+        iterationResultFactory,
+        resultFactory);
+    SafeIterationResult<Token> iterationResult = tokenStream.next();
+    while (iterationResult.isCorrect()) {
+      Token token = iterationResult.iterationResult();
+      Result<OperatorType> opResult = OperatorType.fromSymbol(token.value());
+      if (opResult.isCorrect()) {
+        Result<Deque<ExpressionNode>> newStackResult = generateOperatorNode(opResult.result(), expressionStack);
+        if (!newStackResult.isCorrect()) {
+          return ProcessCheckpoint.UNINITIALIZED();
+        }
+        expressionStack = newStackResult.result();
+      } else {
+        ProcessCheckpoint<Token, ProcessResult<ExpressionNode>> leafResult = leafExpressionNodesParser
+            .parse(tokenStream);
+        if (leafResult.isUninitialized()) {
+          return ProcessCheckpoint.UNINITIALIZED();
         }
 
-        if (expressionStack.size() != 1) {
-            return ProcessCheckpoint.UNINITIALIZED();
+        if (leafResult.result().isComplete()) {
+          expressionStack.push(leafResult.result().result());
+          tokenStream = (TokenStream) leafResult.iterator();
+        } else {
+          break;
         }
-
-        return ProcessCheckpoint.INITIALIZED(tokenStream, expressionStack.pop());
+      }
+      iterationResult = tokenStream.next();
     }
 
-    private Result<Deque<ExpressionNode>> generateOperatorNode(
-            OperatorType operatorType, Deque<ExpressionNode> expressionNodes) {
-        Deque<ExpressionNode> newStack = new ArrayDeque<>(expressionNodes);
-        List<ExpressionNode> children = new ArrayList<>();
-        for (int i = 0; i < operatorType.arity(); i++) {
-            children.add(0, newStack.pop());
-        }
-        OperatorNode operatorNode =
-                nodeFactory.createOperatorNode(
-                        operatorType, children, children.get(0).line(), children.get(0).column());
-        newStack.push(operatorNode);
-        return new CorrectResult<>(newStack);
+    if (expressionStack.size() != 1) {
+      return ProcessCheckpoint.UNINITIALIZED();
     }
+
+    return ProcessCheckpoint.INITIALIZED(tokenStream, expressionStack.pop());
+  }
+
+  private Result<Deque<ExpressionNode>> generateOperatorNode(
+      OperatorType operatorType, Deque<ExpressionNode> expressionNodes) {
+    Deque<ExpressionNode> newStack = new ArrayDeque<>(expressionNodes);
+    List<ExpressionNode> children = new ArrayList<>();
+    for (int i = 0; i < operatorType.arity(); i++) {
+      children.add(0, newStack.pop());
+    }
+    OperatorNode operatorNode = nodeFactory.createOperatorNode(
+        operatorType, children, children.get(0).line(), children.get(0).column());
+    newStack.push(operatorNode);
+    return new CorrectResult<>(newStack);
+  }
 }
