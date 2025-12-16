4cf7e915fca8bd2bd5c77c93c4c6d9450699d2d9
Santiago Garrote <santiagogarrote2005@gmail.com>
2025-12-16 13:40:40 -0300

    .

--- buildSrc/src/main/groovy/PublishingConventionPlugin.groovy
+++ buildSrc/src/main/groovy/PublishingConventionPlugin.groovy
@@ -8,11 +8,17 @@ class PublishingConventionPlugin implements Plugin<Project> {
         project.plugins.apply('java')
         project.plugins.apply('maven-publish')
 
+        // Ensure project.version is set (from -Pversion if provided)
+        if (!project.hasProperty('version') || project.version == 'unspecified') {
+            project.version = '0.0.0-SNAPSHOT'
+        }
+
         project.publishing {
             publications {
                 gpr(MavenPublication) {
                     from project.components.java
                     artifactId = project.name.toLowerCase()
+                    version = project.version
                 }
             }
             repositories {
@@ -20,12 +26,11 @@ class PublishingConventionPlugin implements Plugin<Project> {
                     name = "GitHubPackages"
                     url = project.uri("https://maven.pkg.github.com/IngsisPrintScript/PrintScript")
                     credentials {
-                      username = System.getenv('GITHUB_ACTOR') ?: project.findProperty('gprUser')
-                      password = System.getenv('GITHUB_TOKEN') ?: project.findProperty('gprToken')
+                        username = System.getenv('GITHUB_ACTOR') ?: project.findProperty('gprUser')
+                        password = System.getenv('GITHUB_TOKEN') ?: project.findProperty('gprToken')
                     }
                 }
             }
         }
     }
 }
-
